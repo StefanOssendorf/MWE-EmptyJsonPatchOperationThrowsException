5bd569bef9d3804c75c395d911b019c2ab27b5a3
Stefan Ossendorf <s.ossendorf@kip-deutschland.de>
2020-09-16 09:08:47 +0200

    Initial commit

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MWE_JsonPatchApplyToException.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpPatch]
+        public ActionResult<Foo> Patch([FromBody] JsonPatchDocument<Foo> patchDocument)
+        {
+            var origFoo = new Foo { Name = "Dummy" };
+
+            patchDocument.ApplyTo(origFoo, ModelState); // This should not throw any exception!
+            if ( !ModelState.IsValid )
+            {
+                return ValidationProblem();
+            }
+
+            return Ok(origFoo);
+        }
+    }
+
+    public class Foo
+    {
+        public String Name { get; set; }
+    }
+}
